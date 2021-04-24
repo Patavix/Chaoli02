67ad92d245b06231c6218d56de6b231d6014e2fd
Pan <Tao>
2021-04-24 19:57:44 +0800

    添加了点击tabbaritem的动画

--- Chaoli/TabBarC.swift
+++ Chaoli/TabBarC.swift
@@ -8,13 +8,22 @@
 import UIKit
 import YPImagePicker
 
-
-class TabBarC: UITabBarController, UITabBarControllerDelegate {
-
+class TabBarC: UITabBarController, UITabBarControllerDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
-
         delegate = self
+    }
+    
+    //给tabbaritem设置动画
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        guard let barButtonView = item.value(forKey: "view") as? UIView else { return }
+        
+        let animationLength: TimeInterval = 0.4
+        let propertyAnimator = UIViewPropertyAnimator(duration: animationLength, dampingRatio: 0.5) {
+            barButtonView.transform = CGAffineTransform.identity.scaledBy(x: 0.9, y: 0.9)
+        }
+        propertyAnimator.addAnimations({ barButtonView.transform = .identity }, delayFactor: CGFloat(animationLength))
+        propertyAnimator.startAnimation()
     }
     
     func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
@@ -22,13 +31,10 @@ class TabBarC: UITabBarController, UITabBarControllerDelegate {
             //待做：判断用户是否登陆
             let sb = UIStoryboard(name: "Main", bundle:nil)
             let vc = sb.instantiateViewController(withIdentifier: "NoteVCID") as! NoteVC
-            
+            vc.modalPresentationStyle = .fullScreen
             self.present(vc, animated: true, completion: nil)
-            
             return false
         }
-        
-        
         return true
     }
 
